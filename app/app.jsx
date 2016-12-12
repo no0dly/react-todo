24686a39f068d8a99f7100245f2f2c0b9f785c62
Vadim Derman <thenoodly@gmail.com>
2016-12-11 16:01:50 -0800

    add react router and redirect

--- app/app.jsx
+++ app/app.jsx
@@ -1,29 +1,32 @@
-var React      = require('react');
-var ReactDOM   = require('react-dom');
-var {Provider} = require('react-redux');
-import {Route, Router, IndexRoute, hashHistory} from 'react-router';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import {hashHistory} from 'react-router';
 
 var actions    = require('actions');
 var store      = require('configureStore').configure();
-var TodoAPI    = require('TodoAPI');
 
-import Todo from 'Todo';
-import Login from 'Login';
 import firebase, {firebaseRef} from 'app/firebase';
+import router from 'app/router';
+
+firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+        hashHistory.push('/todos');
+    } else {
+        hashHistory.push('/');
+    }
+})
+
 
 store.dispatch(actions.startAddTodos());
+
 $(document).foundation();
 
 require('style!css!sass!AppCss')
 
 ReactDOM.render(
     <Provider store={store}>
-        <Router history={hashHistory}>
-            <Route path='/'>
-                <Route path='todos' component={Todo} />
-                <IndexRoute component={Login} />
-            </Route>
-        </Router>
+        {router}
     </Provider>,
     document.getElementById('app')
 );
